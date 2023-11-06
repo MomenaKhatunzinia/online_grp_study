a4f401622766785fe40ec24daa4bd3e397929dfa
Your NameMomenaKhatunzinia <momena.ewu@gmail.com>
2023-11-06 19:49:53 +0600

    added creat assignment

--- src/Pages/CreateAssignments/CreatAssignments.jsx
+++ src/Pages/CreateAssignments/CreatAssignments.jsx
@@ -1,15 +1,51 @@
-
+import  { useState } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import swal from "sweetalert";
 
 const CreatAssignments = () => {
+    
+    const [startDate, setStartDate] = useState(new Date());
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [selectOp, setSelectOp] = useState('Difficulty level')
+    const dropDown = () =>
+    {
+        setIsOpen(!isOpen)
+    }
+    const handelDifficulty = (option) =>
+    {
+        setSelectOp(option)
+        setIsOpen(false)
+    }
+
+    const handelSubmit = e =>
+    {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        const thumbnail = form.thumbnail.value;
+        const mark = form.mark.value;
+        console.log(title, description, thumbnail, mark, selectOp,startDate)
+
+        swal("Successfully Added")
+
+        form.reset();
+    }
+
+  
     return (
         <div>
             <h1 className="text-3xl font-bold flex justify-center">Creat An Assignment</h1>
-            <div className="hero  bg-base-200">
+            <div className="hero   lg:p-24 ">
   <div className=" flex-col lg:flex-row-reverse">
     <div className="text-center">
     </div>
     <div className="card flex-shrink-0 max-w-6xl shadow-2xl  ">
-      <form className="card-bod">
+      <form
+      onSubmit={handelSubmit}
+      className="card-bod lg:p-11">
       <div className="form-control">
           <label className="label">
             <span className="label-text">Title</span>
@@ -32,28 +68,46 @@ const CreatAssignments = () => {
 Image URL</span>
           </label>
           <input type="text" placeholder="Thumbnail Image url" 
-          name="thumbnail-image"
+          name="thumbnail"
           className="input input-bordered" required />
         </div>
         <div className="flex gap-12">
+            
         <div className="form-control">
           <label className="label">
             <span className="label-text">Mark</span>
           </label>
-          <input type="text" placeholder="Mark" className="input input-bordered" required />
+          <input type="text" placeholder="Mark" 
+          name="mark"
+          className="input input-bordered" required />
         </div>
-        <div className="form-control">
-        <div className="dropdown dropdown-hover">
-  <label tabIndex={0} className="btn m-1">difficulty leve</label>
+
+        <div className="form-control flex justify-center items-center">
+        <div className="dropdown dropdown-hover ">
+  <label tabIndex={0} className="btn btn-primary  m-1"
+  onClick={dropDown}
+  >{selectOp}</label>
+ { isOpen && (<div>
   <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-    <li><a>Easy</a></li>
-    <li><a>medium</a></li>
-    <li><a>Hard</a></li>
+    <li onClick={()=>handelDifficulty("Easy")}><a>Easy</a></li>
+    <li onClick={()=>handelDifficulty("Medium")}><a>Medium</a></li>
+    <li onClick={()=>handelDifficulty("Hard")}><a>Hard</a></li>
   </ul>
+  </div>)}
 </div>
           
         </div>
         </div>
+
+        <div className="form-control mt-7 flex justify-center items-center">
+
+        <DatePicker
+      showIcon
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+    />
+        </div>
+
         <div className="form-control mt-6">
           <button className="btn btn-primary">Submit</button>
         </div>
@@ -61,6 +115,8 @@ Image URL</span>
     </div>
   </div>
 </div>
+
+
            
 
         </div>
